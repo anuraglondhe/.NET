5db367e6de087c6d49ffe36c313188c87dc2c32b
Anurag Londhe <anuraglondhe9052.com>
2025-12-10 17:17:13 +0530

    assignment1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Student Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Roll Number: ");
+            int rollNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your course name: ");
+            string course = Console.ReadLine();
+        }
+       
+    }
+}
