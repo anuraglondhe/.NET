19589955228c0da6abb83c1798829e4e5454c646
Anurag Londhe <anuraglondhe9052.com>
2025-12-31 06:53:09 +0530

    changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,18 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Student Name: ");
-            string name = Console.ReadLine();
+            Student s1 = new Student();
 
-            Console.WriteLine("Enter Roll Number: ");
-            int rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Name: ");
+            s1.Name = Console.ReadLine();
 
-            Console.WriteLine("Enter your Age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Age: ");
+            s1.Age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter your course name: ");
-            string course = Console.ReadLine();
+            Console.Write("Roll No: ");
+            s1.RollNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Course: ");
+            s1.Course = Console.ReadLine();
+
+
+            // Display Student Info
+
+            Console.WriteLine($"Name: {s1.Name}");
+            Console.WriteLine($"Age: {s1.Age}");
+            Console.WriteLine($"Roll No: {s1.RollNo}");
+            Console.WriteLine($"Course: {s1.Course}");
         }
-       
+    }
+
+    class Student
+    {
+        private string _name;
+        private int _age;
+        private int _rollNo;
+        private string _course;
+
+        public string Course
+        {
+            get { return _course; }
+            set { _course = value; }
+        }
+
+
+        public int RollNo
+        {
+            get { return _rollNo; }
+            set { _rollNo = value; }
+        }
+
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
     }
 }
